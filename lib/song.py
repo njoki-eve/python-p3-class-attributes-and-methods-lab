08b4bde087efbd967c75d0b9510f157659cecc2a
njoki-eve <njokieverlyne71@gmail.com>
2025-05-24 03:29:16 -0700

    first commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Update total count
+        Song.count += 1
+        
+        # Update genres list and count
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+        
+        # Update artists list and count
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
